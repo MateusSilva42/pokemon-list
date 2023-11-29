7eac4cf3c37b2cd01b76d4d23afea1b63ea5bf4b
MateusSilva42 <mateus42silva@gmail.com>
2023-11-29 17:04:41 -0300

    get back to old routes config

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,20 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';
-import PokemonPage from '../pages/Pokemon';
-import Error404 from '../pages/404';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "../pages/Home";
+import PokemonPage from "../pages/Pokemon"
+import Error404 from "../pages/404"
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <Error404 />,
+  },
+  {
+    path: "/pokemon",
+    element: <PokemonPage />,
+    children: [
+      {
+        path: ":id",
+        element: <PokemonPage />,
+      },
+    ],
+  }
+]);
 
 function AppRoutes() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pokemon" element={<PokemonPage />} />
-        <Route path="/pokemon/:id" element={<PokemonPage />} />
-        <Route path="/404" element={<Error404 />} />
-        <Route path="*" element={<Error404 />} />
-      </Routes>
-    </Router>
-  );
+  return <RouterProvider router={routes} />;
 }
 
 export default AppRoutes;
